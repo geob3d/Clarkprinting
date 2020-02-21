8d92d3e6ff0a55eb4883ca614d3ce664e5da8122
geob3d <30846371+geob3d@users.noreply.github.com>
2020-02-21 00:16:03 -0500

    images

--- app/ImageCoordinates.php
+++ app/ImageCoordinates.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ImageCoordinates extends Model
 {
-    //
+    protected $fillable = ['model_id', 'row_id','field_name','field_type','x_coordinate','y_coordinate','scaleX','width','height','angle'];
 }
+
+
